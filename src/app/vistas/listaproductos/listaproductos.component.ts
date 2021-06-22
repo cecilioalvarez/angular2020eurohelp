1a907c89bffcc4aec0d6ab2360caea74cacd1373
cecilioAlvarez <cmac@arquitecturajava.com>
2021-06-22 12:20:30 +0200

    editar detalle

--- src/app/vistas/listaproductos/listaproductos.component.ts
+++ src/app/vistas/listaproductos/listaproductos.component.ts
@@ -11,14 +11,33 @@ export class ListaproductosComponent implements OnInit {
   //me declaro la variable
   listaProductos:Producto[];
   nuevo:Producto;
+  seleccionado:Producto;
+  vistaActiva:string="listaProductos"
   //inyeccion de dependencia 
   // con angular como framework
 
   constructor(public productoService:ProductosService) { 
     this.nuevo= {} as Producto;
+    this.seleccionado={} as Producto;
     // porque ya no es el componente el encargado de gestionar el producto
     // sino que lo hacemos a traves de una lista
    this.listaProductos=productoService.buscarTodos();
+
+  }
+  editarProducto(producto:Producto) {
+    this.vistaActiva="editarProductos";
+    this.seleccionado=producto;
+  }
+  nuevoProducto() {
+      this.vistaActiva="formularioProductos";
+  }
+  salvar() {
+    this.vistaActiva="listaProductos";
+  }
+  verDetalle(producto:Producto) {
+    
+    this.vistaActiva="detalle";
+    this.seleccionado=producto;
 
   }
   // el que esta en el componente
@@ -27,6 +46,7 @@ export class ListaproductosComponent implements OnInit {
   
     this.productoService.insertar(this.nuevo);
     this.nuevo={} as Producto;
+    this.vistaActiva="listaProductos";
 
   }
   borrar(producto:Producto) {
