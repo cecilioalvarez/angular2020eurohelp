95a06f62a59cdd80e22b09cfc2936b8df7ca0dd1
cecilioAlvarez <cmac@arquitecturajava.com>
2021-06-22 09:19:27 +0200

    select final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
 import { C3Component } from './c3/c3.component';
 import { C4Component } from './c4/c4.component';
+import { C5Component } from './c5/c5.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { C4Component } from './c4/c4.component';
     C1Component,
     C2Component,
     C3Component,
-    C4Component
+    C4Component,
+    C5Component
   ],
   imports: [
     BrowserModule,
